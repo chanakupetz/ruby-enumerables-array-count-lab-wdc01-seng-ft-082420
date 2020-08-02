efa13f7fe168b1d5ab3614eb9f1c915eddaba796
Chana Kupetz <chanakupetz@gmail.com>
2020-08-02 02:52:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
     array.count(element)
-      element.string?
+      element == string?
   # Return the total number of strings in the provided array using the count enumerable
 end
 
